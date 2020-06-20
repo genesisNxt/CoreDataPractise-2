77a1cb49dacf7fd0ffc06fa13442499757fbb5f5
parmjit <engg.parmjit@gmail.com>
2020-06-20 15:26:58 +1000

    Initial Commit

---
+++ CoreDataPractise 2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreDataPractise 2
+//
+//  Created by PARMJIT SINGH KHATTRA on 20/6/20.
+//  Copyright © 2020 PARMJIT SINGH KHATTRA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
